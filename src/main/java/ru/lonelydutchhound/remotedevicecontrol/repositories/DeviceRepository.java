6870c2058efc6f39cfc9aa4fbe29d12a7322888b
Elena <53603781+LonelyDutchhound@users.noreply.github.com>
2021-10-28 00:05:58 +0300

    Add DTOs and enum type util
    - DTOs
    - util for enum type

---
+++ src/main/java/ru/lonelydutchhound/remotedevicecontrol/repositories/DeviceRepository.java
@@ -0,0 +1,11 @@
+package ru.lonelydutchhound.remotedevicecontrol.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.lonelydutchhound.remotedevicecontrol.models.WashingMachineDevice;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface DeviceRepository extends JpaRepository<WashingMachineDevice, UUID> {
+    List<WashingMachineDevice> findAllByDeletedAtIsNull();
+}
