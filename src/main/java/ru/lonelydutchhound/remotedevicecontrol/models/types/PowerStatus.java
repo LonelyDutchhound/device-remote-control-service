2f09e1468c29ee9aefd076804cfe6bec7b02ef20
LonelyDutchhound <lonely.dutchhound@gmail.com>
2021-10-27 23:58:13 +0300

    add: - model - migrations

---
+++ src/main/java/ru/lonelydutchhound/remotedevicecontrol/models/types/PowerStatus.java
@@ -0,0 +1,6 @@
+package ru.lonelydutchhound.remotedevicecontrol.models.types;
+
+public enum PowerStatus {
+    ON,
+    OFF
+}
---
+++ src/main/java/ru/lonelydutchhound/remotedevicecontrol/models/types/ProgramStatus.java
@@ -0,0 +1,8 @@
+package ru.lonelydutchhound.remotedevicecontrol.models.types;
+
+public enum ProgramStatus {
+    STARTING,
+    RUNNING,
+    FINISHED,
+    ERROR
+}
